60deb77de68a2e54885ff81224fbfbbebf6d155b
LeonCF <1037299165@qq.com>
2018-12-25 20:18:16 +0800

    project architecture 1.0.0

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'mirrorx';
+import {HashRouter} from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+    <HashRouter basename='/'>
+        <App/>
+    </HashRouter>,
+  document.getElementById('root')
+);
+
+window.getSelection() ? window.getSelection().removeAllRanges() : document.selection.empty(); //这句话干啥用的
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
