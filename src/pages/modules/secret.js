9ce2460084534435979ad2dfb583c526260fa16c
LeonCF <1037299165@qq.com>
2019-01-02 23:33:39 +0800

    correct ui

--- src/pages/modules/secret.js
+++ src/pages/modules/secret.js
@@ -3,13 +3,24 @@ import React, { Component } from 'react';
 class Secret extends Component{
     constructor() {
         super();
+        this.state={
+            fileValue: '未选择任何文件'
+        }
+        this._fileChange = this.fileChange.bind(this);
     }
 
+    fileChange(e) {
+        this.setState({fileValue: e.target.value});
+    }
     render(){
         return(
-            <div className="secret-main">
-                <label className="secret-main__label">Browse documents</label>
-                <input type="file" name="apkBeforeSign"/>
+            <div className="file">
+                <label htmlFor="file">文件：</label>
+                <div className="userdefined-file">
+                　　<input type="text" name="userdefinedFile" id="userdefinedFile" value={ this.state.fileValue } />
+                　　<button type="button">上传</button>
+                </div>
+                <input type="file" name="file" id="file" onChange={ this._fileChange }/>            
             </div>
         )
     }
